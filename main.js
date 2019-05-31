9f5cdbea57c1c37321fe63911f733dfbb34f4d87
vitalii <dumavb777@gmail.com>
2019-05-31 21:40:14 +0300

    sol

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
-function daysInMonth(month, year) {
-    try {
-        if (!Number.isInteger(year)
-            || !Number.isInteger(month)
-            || month > 12
-        ) {
-            throw new SyntaxError("not a positive integer");
-        }
 
-        return new Date(year, month, 0).getDate();
-    } catch(e) {
-        console.log(e);
-    }
-}
